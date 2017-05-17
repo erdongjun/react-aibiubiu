de3d537f39fedcdb6db8c1fde39532061fcd924e
xifan <993656553@qq.com>
2017-05-17 21:13:13 +0800

    add action logger

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,12 +1,17 @@
 import { createStore,applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
 import thunk from 'redux-thunk';  //redux 异步 action 中间件
+import {createLogger} from 'redux-logger';
+
+const logger = createLogger();
 
 
-console.log(applyMiddleware,thunk)
+
+
+console.log(createLogger)
 
 export default function configureStore(initialState) {
-    const store = createStore(rootReducer, initialState,applyMiddleware(thunk),
+    const store = createStore(rootReducer, initialState, applyMiddleware(thunk ,logger),
         // 触发 redux-devtools
         window.devToolsExtension ? window.devToolsExtension() : undefined
         
